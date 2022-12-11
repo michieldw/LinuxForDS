ed4552f428bf3fe2b038ae40161634ed1ea0cfa2
Michiel DW <michiel.dewilde@hotmail.com>
2022-12-11 23:43:42 +0100

    final?

--- data-workflow/analyse.py
+++ data-workflow/analyse.py
@@ -1,23 +1,37 @@
 
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # data inladen
-data = np.loadtxt('out.csv', delimiter=';', skiprows=1,
+data = np.loadtxt('out.csv', delimiter=',', skiprows=1,
                   dtype={'names': ('date', 'BNB', 'BTC', 'ETH', 'USDC', 'USDT'),
-                         'formats': ('S10', 'f4', 'f4', 'f4', 'f4', 'f4')})
+                         'formats': ('S15', 'f4', 'f4', 'f4', 'f4', 'f4')})
 
 date = data['date']
+
+d = {}
+
+
 
 for col_name in ['BNB', 'BTC', 'ETH', 'USDC', 'USDT']:
     current_column = data[col_name]
     cur_mean = np.mean(current_column)
     cur_std = np.std(current_column)
+
+    d[col_name] = f"mean={cur_mean},standard deviation={cur_std}"
     
     plt.plot(date, current_column)
     plt.xlabel('Time')
-    plt.ylabel(col_name)
-    plt.title(f"Price of ${col_name} over time")
-    plt.show()
+    plt.ylabel(f"{col_name} in USD")
+    plt.title(f"Price of {col_name} over time")
+    plt.savefig(f"{col_name}.png")
+    plt.clf()
+
+with open('./data.txt', 'r+') as file:
+    file.seek(0)
+    file.truncate()
+    for currency,stats in d.items():
+        file.write(f"{currency}\t\t{stats}\n")
+
 
---
+++ data-workflow/report.md
@@ -0,0 +1,28 @@
+# Reporting on cryptocurrency data. zo 11 dec 2022 23:39:27 CET
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+## BNB
+
+![BNB](./BNB.png)
+
+## BTC
+
+![BTC](./BTC.png)
+
+## ETH
+
+![ETH](./ETH.png)
+
+## USDC
+
+![USDC](./USDC.png)
+
+## USDT
+
+![USDT](./USDT.png)
+
+## Conclusion
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
