fdf58c7337cfba11f8eef9903b356584118999d7
Michiel DW <michiel.dewilde@hotmail.com>
2022-12-11 21:59:40 +0100

    single line mapping complete

--- data-workflow/transform.sh
+++ data-workflow/transform.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-BASE_DIR=(dirname $0)
+BASE_DIR=$(dirname "$0")
 
-cat data/data-20221211-101001.json |
-  jq '.data |
-  map_values(.[0]) |
-  map_values(.description) |
-  map_values(. |= sub("^.* (?<price>[^ ]+) USD.*$"; "\(.price)"))' \
-  > temp.csv
+
+
+
+
+# cat data/data-20221211-101001.json |
+#   jq -r '.data |
+#   map_values(.[0]) |
+#   map_values(.description) |
+#   map_values(. |= sub("^.* (?<price>[^ ]+) USD.*$"; "\(.price)")) |
+#   .[]' |
+#   tr '\n' ';' \
+#   > temp.csv
 
 #'.taskDefinition.containerDefinitions[].image |= sub("(?<repo>.*:).*"; "\(.repo)new")'
