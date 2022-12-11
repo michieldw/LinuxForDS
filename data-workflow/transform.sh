e6fb10fbbd26f2820a4b5c8259576231308e13f4
Michiel DW <michiel.dewilde@hotmail.com>
2022-12-11 22:39:33 +0100

    Transform afgewerkt

--- data-workflow/transform.sh
+++ data-workflow/transform.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-BASE_DIR=$(dirname "$0")
+BASE_DIR=$(dirname "$0") #zorgen dat paden juist werken indien script niet vanuit data-workflow/ uitgevoerd wordt
 
+touch $BASE_DIR/out.csv #outputbestand aanmaken indien het nog niet bestaat
+echo "DATETIME;BNB;BTC;ETH;USDC;USDT" > $BASE_DIR/out.csv #hoofding van csv bestand maken
 
+for FILE in $BASE_DIR/data/*.json; #over paden van elke file onder data-workflow/data/ loopen
+do
+  DATETIME=$(echo $FILE | grep -Eo '[[:digit:]]{8}-[[:digit:]]{6}') # datetime uit filenaam halen (https://stackoverflow.com/questions/26881104/extract-date-from-a-file-name-in-unix-using-shell-scripting)
+  cat $FILE | #jsonbestand pipen naar jq commando
+    jq -r '.data | 
+    map_values(.[0]) |
+    map_values(.description) |
+    map_values(. |= sub("^.* (?<price>[^ ]+) USD.*$"; "\(.price)")) |
+    .[]' | #elke file omzetten naar een lijst van 5 prijzen in USD
+    tr -d ',' | #komma's verwijderen
+    tr '.' ',' | #punten omzetten in komma's (Europees formaat voor csv-bestand)
+    tr '\n' ';' | #newlines omzetten in puntkomma's
+    sed "s/^/$DATETIME;/" \
+    >> $BASE_DIR/out.csv #csv-lijn voor huidig bestand achteraan het outputbestand toevoegen
+done
 
-
-
-# cat data/data-20221211-101001.json |
-#   jq -r '.data |
-#   map_values(.[0]) |
-#   map_values(.description) |
-#   map_values(. |= sub("^.* (?<price>[^ ]+) USD.*$"; "\(.price)")) |
-#   .[]' |
-#   tr '\n' ';' \
-#   > temp.csv
-
-#'.taskDefinition.containerDefinitions[].image |= sub("(?<repo>.*:).*"; "\(.repo)new")'
